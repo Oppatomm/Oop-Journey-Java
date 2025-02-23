b9fb1af8da81343b397efb73c933bfe7cbcc7824
Sumate Boonkird <sumateboonkird5555github@gmail.com>
2025-02-23 23:44:45 +0700

    Add File : Calculate_the_Area_of_Geometric_Shapes_Using_Inheritance.java

---
+++ projects/small_projects/Calculate_the_Area_of_Geometric_Shapes_Using_Inheritance.java
@@ -0,0 +1,55 @@
+abstract class Shape {
+    abstract void calculateArea();
+}
+
+class Rectangle extends Shape {
+    private int width;
+    private int height;
+    public Rectangle (int width , int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth () {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public void calculateArea() {
+        int rectangleArea = getWidth() * getHeight();
+        System.out.println("Rectangle : " + rectangleArea);
+    }
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    Circle (double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius () {
+        return radius;
+    }
+
+    @Override
+    public void calculateArea() {
+        double circleArea = Math.PI * (getRadius() * getRadius());
+        System.out.printf("Circle : %.2f\n" , circleArea);
+    }
+}
+
+public class Calculate_the_Area_of_Geometric_Shapes_Using_Inheritance {
+    public static void main (String [] args) {
+
+        Rectangle rec = new Rectangle(12, 12);
+        rec.calculateArea();
+
+        Circle circle = new Circle(12);
+        circle.calculateArea();
+    }
+}
